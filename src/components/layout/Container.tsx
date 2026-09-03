d4f631865482d53f7b7c13dd1a3b8eb9f9918755
João Paulo Araújo <jpaulofonseca99@gmail.com>
2026-09-02 21:06:02 -0300

    feat: add layout architecture with scrollable grid area

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -6,14 +6,16 @@ const Container = () => {
   const [filters, setFilters] = useState<Omit<CardFilters, 'num' | 'offset'>>({})
 
   return (
-    <main className="flex flex-col items-center w-full gap-6">
-      <header className="mb-6">
+    <main className="flex flex-col gap-6 w-full h-full p-6">
+      <header>
         <h1 className="text-2xl font-outlier font-bold text-ink">
           TCG Market
         </h1>
       </header>
 
-      <CardGrid filters={filters}/>
+      <div className='flex flex-1 min-h-0 overflow-hidden'>
+        <CardGrid filters={filters} />
+      </div>
     </main>
   )
 }
